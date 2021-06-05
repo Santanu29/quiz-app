8b85fee5b57b95b49bcd745e2e7ddc67ab3038ca
Santanu Biswas <69161246+Santanu29@users.noreply.github.com>
2021-06-05 12:31:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const quizData = [
+    {
+        question: "How many grams in 1 kilogram",
+        a: '100',
+        b: '10000',
+        c: '1000', 
+        d: '500',
+        correct: 'c'
+    } , {
+        question: "What is the most popular programming language in 2020",
+        a: 'Python',
+        b: 'JavaScript',
+        c: 'C++',
+        d: 'Java',
+        correct: 'b'
+    } , {
+        question: "What does HTML stand for?",
+        a: 'Highly Translated Machine Language',
+        b: 'Hyper Transparent Marked Language',
+        c: 'Hexa Typical Medium Language',
+        d: 'Hyper Text Markup Language',
+        correct: 'd'
+    } , {
+        question: "What year was JavaScript launched?",
+        a: '1998',
+        b: '1995',
+        c: '2000',
+        d: '1990',
+        correct: 'b'
+    }
+]
